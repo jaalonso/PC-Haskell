3080c2049011115bde016cbb0c02565d107d62cd
Jose A. Alonso <JoseA.Alonso@gmail.com>
2017-12-18 13:59:53 +0100

    Ejemplo de aritmética modular: factorial módulo

--- src/Cap_1/FactorialModulo.hs
+++ src/Cap_1/FactorialModulo.hs
@@ -1,58 +1,54 @@
--- |
--- Module      : FactorialModulo
--- Description : Factorial módulo.
--- License     : GPL-3
--- Maintainer  : José A. Alonso
--- 
--- __Factorial módulo__
---
--- Se comparan dos definiciones de factorial con módulo para mostrar que
--- es más eficiente usar los módulos durante el cálculo.
+-- FactorialModulo.hs
+-- Factorial módulo.
+-- José A. Alonso Jiménez <jalonso@us.es>
+-- Sevilla, 18 de Diciembre de 2017
+-- ---------------------------------------------------------------------
 
 module Cap_1.FactorialModulo where
 
--- | __(factorialMod n x)__ es el factorial de x módulo n. Por ejemplo,
---
--- >>> factorialMod (7+10^9) 100
--- 437918130
+-- ---------------------------------------------------------------------
+-- Definir la función
+--    factorialMod :: Integer -> Integer -> Integer
+-- tal que (factorialMod n x) es el factorial de x módulo n. Por ejemplo,
+--    factorialMod (7+10^9) 100       ==  437918130
+--    factorialMod (7+10^9) (5*10^6)  ==  974067448
+-- ---------------------------------------------------------------------
+
+import Data.List (foldl')
+
+-- 1ª definición
+-- =============
+
 factorialMod :: Integer -> Integer -> Integer
 factorialMod n x =
   factorial x `mod` n
 
--- | __(factorial x)__ es el factorial de x. Por ejemplo,
---
--- >>> factorial 3
--- 6
+-- (factorial x) es el factorial de x. Por ejemplo,
+--    factorial 3  ==  6
 factorial :: Integer -> Integer
 factorial x = product [1..x]
 
--- | 2ª definición.
---
--- >>> factorialMod2 (7+10^9) 100
--- 437918130
+-- 2ª definición
+-- =============
+
 factorialMod2 :: Integer -> Integer -> Integer
 factorialMod2 n x =
-  foldr (prodMod n) 1 [1..x]
+  foldl' (prodMod n) 1 [1..x]
 
--- | __(prodMod n x y)__ es el producto de x e y módulo n. Por ejemplo,
---
--- >>> prodMod 3 4 7
--- 1
--- >>> prodMod 5 4 7
--- 3
+-- (prodMod n x y) es el producto de x e y módulo n. Por ejemplo,
+--    prodMod 3 4 7  ==  1
+--    prodMod 5 4 7  ==  -- 3
 prodMod :: Integer -> Integer -> Integer -> Integer
 prodMod n x y =
   ((x `mod` n) * (y `mod` n)) `mod` n
   
--- $doc
---
--- __Comparación de eficiencia__
---
--- > > factorialMod (7+10^9) (3*10^4)
--- > 548996970
--- > (2.95 secs, 879,781,488 bytes)
---
--- > > factorialMod2 (7+10^9) (3*10^4)
--- > 548996970
--- > (0.12 secs, 14,188,936 bytes)
+-- Comparación de eficiencia
+-- =========================
+
+--    λ> factorialMod (7+10^9) (5*10^4)
+--    737935835
+--    (2.62 secs, 2,601,358,640 bytes)
+--    λ> factorialMod2 (7+10^9) (5*10^4)
+--    737935835
+--    (0.07 secs, 23,471,880 bytes)
 
